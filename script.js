999e1c6069fe4c01208634b8738372ea36507995
indratrst <tristia71@gmail.com>
2022-08-22 16:53:47 +0700

    update 1

--- script.js
+++ script.js
@@ -1,46 +1,77 @@
-$(".search-btn").on("click", function () {
-  $.ajax({
-    url: "http://www.omdbapi.com/?apikey=2bde9316&s=" + $(".input-keyword").val(),
-    success: (results) => {
-      const movies = results.Search;
-      let cards = "";
-      movies.forEach((m) => {
-        cards += showCards(m);
-      });
-      $(".movie-container").html(cards);
+// $(".search-btn").on("click", function () {
+//   $.ajax({
+//     url: "http://www.omdbapi.com/?apikey=2bde9316&s=" + $(".input-keyword").val(),
+//     success: (results) => {
+//       const movies = results.Search;
+//       let cards = "";
+//       movies.forEach((m) => {
+//         cards += showCards(m);
+//       });
+//       $(".movie-container").html(cards);
 
-      // click btn modal
-      $(".modal-detail-btn").on("click", function () {
-        // console.log($(this).data("imdbid"));
-        $.ajax({
-          url: "http://www.omdbapi.com/?apikey=2bde9316&i=" + $(this).data("imdbid"),
-          success: (m) => {
+//       // click btn modal
+//       $(".modal-detail-btn").on("click", function () {
+//         // console.log($(this).data("imdbid"));
+//         $.ajax({
+//           url: "http://www.omdbapi.com/?apikey=2bde9316&i=" + $(this).data("imdbid"),
+//           success: (m) => {
+//             const movieDetail = showMovieDetail(m);
+//             $(".modal-body").html(movieDetail);
+//           },
+//           error: (e) => {
+//             console.log(e.responseText);
+//           },
+//         });
+//       });
+//     },
+//     error: (e) => {
+//       console.log(e.responseText);
+//     },
+//   });
+// });
+
+// fetch
+const searchBtn = document.querySelector(".search-btn");
+searchBtn.addEventListener("click", function () {
+  const inputKeyword = document.querySelector(".input-keyword");
+  fetch("http://www.omdbapi.com/?apikey=2bde9316&s=" + inputKeyword.value)
+	.then((response) => response.json())
+  .then(response => {
+    const movies = response.Search;
+    let cards = "";
+    movies.forEach(m => cards += showCards(m));
+      const movieContainer = document.querySelector(".movie-container");
+      movieContainer.innerHTML = cards ;
+
+      //  ketika tombol di klik 
+      const modalDetailBtn = document.querySelectorAll(".modal-detail-btn");
+      modalDetailBtn.forEach(btn => {
+        btn.addEventListener("click", function () {
+          const imdbid = this.dataset.imdbid;
+          fetch("http://www.omdbapi.com/?apikey=2bde9316&i=" + imdbid)
+          .then(response => response.json())
+          .then(m => {
             const movieDetail = showMovieDetail(m);
-            $(".modal-body").html(movieDetail);
-          },
-          error: (e) => {
-            console.log(e.responseText);
-          },
-        });
+            const modalBody = document.querySelector(".modal-body");
+            modalBody.innerHTML = movieDetail;
+          });
+        })
       });
-    },
-    error: (e) => {
-      console.log(e.responseText);
-    },
+    
   });
 });
 
 function showCards(m) {
   return `<div class="col-md-4 my-3">
-  <div class="card">
-    <img src="${m.Poster}" class="card-img-top" />
-    <div class="card-body">
-      <h5 class="card-title">${m.Title}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-      <a href="#" class="btn btn-primary modal-detail-btn" data-toggle="modal" data-target="#movieDetailModal" data-imdbid=${m.imdbID}>Movie Details</a>
+    <div class="card">
+      <img src="${m.Poster}" class="card-img-top" />
+      <div class="card-body">
+        <h5 class="card-title">${m.Title}</h5>
+        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+        <a href="#" class="btn btn-primary modal-detail-btn" data-toggle="modal" data-target="#movieDetailModal" data-imdbid=${m.imdbID}>Movie Details</a>
+      </div>
     </div>
-  </div>
-</div>`;
+  </div>`;
 }
 
 function showMovieDetail(m) {
